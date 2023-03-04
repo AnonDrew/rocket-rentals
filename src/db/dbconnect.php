9864decbec66f75cc48c4731f705b170f03ae2aa
AnonDrew <segdrew328@gmail.com>
2023-03-04 08:07:30 -0500

    containers finalized. fixed php mysqli hostname param.

--- src/db/dbconnect.php
+++ src/db/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-//tweaked; also fixed typos; from Professor Miller:
+//tweaked; also fixed typos; from Professor M.:
 
-$DB_USERNAME = "asegu51498"; //Your userid for tophat/database server, this is case-sensitive
-$DB_DATABASE = $DB_USERNAME . "_webgroup1"; //Name of your database, the default is yourusername_default, this is case sensitive
+$DB_USERNAME = "webgroup1"; //Your userid for tophat/database server, this is case-sensitive
+$DB_DATABASE = "rocket_rentals"; //Name of your database, the default is yourusername_default, this is case sensitive
 
 $dbpwdPath = "/home/students/" . $DB_USERNAME . "/db.txt";
 $db = null; //Mysqli Object
@@ -12,7 +12,7 @@ if (file_exists($dbpwdPath)) {
 	 $DBPWD = trim(file_get_contents($dbpwdPath));
 
      //instead of db, use localhost for tophat
-	 $db = new mysqli("webgroup1_db_1", $DB_USERNAME, $DBPWD, $DB_DATABASE);
+	 $db = new mysqli("db", $DB_USERNAME, $DBPWD, $DB_DATABASE, 3306);
 	 if ($db->connect_errno) {
 		  echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
 	 }
