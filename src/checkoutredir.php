65bbf99d1abc82ac6e7d9993d38ccd4997a4635d
AnonDrew <segdrew328@gmail.com>
2023-03-06 01:44:39 -0500

    tweaked formatting and login path. added echo for query failure. fixed header error.

--- src/checkoutredir.php
+++ src/checkoutredir.php
@@ -4,7 +4,7 @@ require_once("db/dbconnect.php");
 session_start();
 
 if (!isLoggedIn()){
-    header ("Location /auth/login.php", true);
+    header ("Location auth/login.php", true);
 }
 
 date_default_timezone_set('EST');
@@ -14,13 +14,22 @@ $formatted_return_date = $return_date->format('Y-m-d H:i:s');
 
 $db->autocommit(FALSE);
 
-$sql = sprintf("UPDATE cars SET is_rented='%s', return_date='%s' WHERE cid='%d'", 
-$db->real_escape_string($_SESSION['username']), $db->real_escape_string($formatted_return_date), $db->real_escape_string($_GET['cid']));
-echo $formatted_return_date;
+$sql = sprintf(
+    "UPDATE cars SET rented_by='%s', return_date='%s' WHERE cid='%d'", 
+    $db->real_escape_string($_SESSION['username']),
+    $db->real_escape_string($formatted_return_date),
+    $db->real_escape_string($_GET['cid'])
+);
+
+//Causes “Cannot Modify Header Information – Headers Already Sent By” Error??
+//echo $formatted_return_date;
 
 if ($db->query($sql)){
     $db->commit();
     header ("Location: index.php", true);
+}
+else {
+    echo "failed";
 }
 
 $db->close();
